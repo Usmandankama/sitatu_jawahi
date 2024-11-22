41afa325d3304b395ed3dde97ae419734a9101ee
Usman Abubakar <104586608+Usmandankama@users.noreply.github.com>
2024-11-22 11:23:40 +0100

    Create colors.dart

---
+++ lib/constants/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+// Color primaryColor = const Color.fromARGB(255, 255, 255, 255);
+Color primaryColor = const  Color(0xFFF2C078);
+Color tileColor = Color.fromARGB(255, 214, 166, 99);
+Color fontColorLight = const Color.fromARGB(179, 255, 255, 255);
