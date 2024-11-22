7c3b2b8e2cad1648fe6b3afbaccca9caf34a56e5
Usman Abubakar <104586608+Usmandankama@users.noreply.github.com>
2024-11-22 11:23:19 +0100

    Create hadith_books.dart

---
+++ lib/models/hadith_books.dart
@@ -0,0 +1,16 @@
+class HadithBooks{
+  String length;
+  String titleArabic;
+  String titleEnglish;
+  String authorEnglish;
+  String authorArabic;
+  // List<dynamic> hadithContent;
+  HadithBooks ({
+    required this.authorArabic,
+    required this.authorEnglish,
+    required this.length,
+    required this.titleArabic,
+    required this.titleEnglish,
+    // required this.hadithContent
+  });
+}
