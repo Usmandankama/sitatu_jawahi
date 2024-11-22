113bc20728be14fe86dd366501e0c85b9b09da6b
Usman Abubakar <104586608+Usmandankama@users.noreply.github.com>
2024-11-22 11:23:22 +0100

    Create hadith.dart

---
+++ lib/models/hadith.dart
@@ -0,0 +1,12 @@
+class Hadith {
+  String arabic;
+  String english;
+  String narrator;
+  int id;
+  Hadith({
+    required this.arabic,
+    required this.english,
+    required this.narrator,
+    required this.id,
+  });
+}
